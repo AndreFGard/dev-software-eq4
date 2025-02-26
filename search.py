582ec6431ce496fa83daca634127739367035528
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2025-02-26 14:29:47 -0300

    add ragscraper basic scraping

--- search.py
+++ search.py
@@ -34,8 +34,8 @@ class Searcher:
     async def search(self, query):
         if self.use_demo:
             if not self.demo: self.demo = getDemoResults()
-            return self.demo
-        else: return await [SearchItem(**result) for result in self._search_brave(query)['web']['results']]
+            return [SearchItem(**result) for result in self.demo['web']['results']]
+        else: return [SearchItem(**result) for result in await self._search_brave(query)['web']['results']]
    
     async def _search_brave(self, query: str, country="BR"):
         headers = {
@@ -53,15 +53,26 @@ class Searcher:
             async with session.get(search_url, headers=headers, params=params) as response:
                 return await response.json()
 
+from rag_scraper.scraper import Scraper
+from rag_scraper.converter import Converter
 
-
-
-
-
-async def main():
-    #results = await search_brave("what to do in olinda as a tourist")
-    s = Searcher("")
-    print(await s.search("eae"))
-
-if __name__ == "__main__":
-    asyncio.run(main())
+class RAG:
+    def __init__(self, brave_api_key=""):
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Connection': 'keep-alive',
+        }
+        self.sr = Searcher(brave_api_key)
+    
+    async def search_and_scrap(self, query: str):
+        results = await self.sr.search(query)
+        for r in results:
+            html = Scraper.fetch_html(r.url, headers = self.headers)
+            md = markdown_content = Converter.html_to_markdown(html=html, parser_features='html.parser',ignore_links=True)
+            print(md)
+            break
+            
+r = RAG()
+asyncio.run(r.search_and_scrap(""))
