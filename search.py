b4f3db6460c3dd91e8c1f2f552c1d151847045cd
Andrefgard <96950645+AndreFGard@users.noreply.github.com>
2025-03-10 15:01:19 -0300

    fix searcher

--- search.py
+++ search.py
@@ -2,8 +2,8 @@ import asyncio
 import aiohttp
 
 from  pydantic import BaseModel
+brave_api_key = ""  # Add your Brave API key here
 search_url = "https://api.search.brave.com/res/v1/web/search"
-brave_api_key = "BSAsV5w2ABc9SshOY3pMZUc2m3eOfd8"  # Add your Brave API key here
 
 def _update_demo(fname="searchdemo.json"):
     f = open(fname,"w")
@@ -42,7 +42,9 @@ class Searcher:
         if self.use_demo:
             if not self.demo: self.demo = getDemoResults()
             return [SearchItem(**result) for result in self.demo['web']['results']]
-        else: return [SearchItem(**result) for result in await self._search_brave(query)['web']['results']]
+        else:
+            results = await self._search_brave(query)
+            return [SearchItem(**result) for result in results['web']['results']]
    
     async def _search_brave(self, query: str, country="BR"):
         headers = {
@@ -55,9 +57,9 @@ class Searcher:
             "q": query,
             "country": country
         }
-        
+        search_url = "https://api.search.brave.com/res/v1/web/search"
+
         async with aiohttp.ClientSession() as session:
             async with session.get(search_url, headers=headers, params=params) as response:
                 return await response.json()
 
-_update_demo()
