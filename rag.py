ff5fbcb44f35b2ccee0734c530f4195e430e68ca
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2025-03-13 21:12:45 -0300

    docs: reduce docstring

--- rag.py
+++ rag.py
@@ -44,15 +44,7 @@ class SlidingWindowChunking:
     """
     A class used to perform sliding window chunking on text data.
     This class provides methods to split text into chunks of a specified window size
-    with a given step size. It also includes functionality to summarize text using
-    an external language model.
-    Methods
-    -------
-    chunk(text)
-        Splits the input text into chunks based on the window size and step size.
-    _add_chunks(site)
-        Asynchronously processes a CrawlResult object to generate markdown, summarize it,
-        and split it into chunks.
+    with a given step size. It also summarizes with an LLM if possible and filters the markdown
     """
 
     def __init__(self, window_size=100, step=35, openai_key=os.environ.get('OPENAI_KEY') or 'INVALID'):
@@ -76,9 +68,6 @@ class SlidingWindowChunking:
         """
         Asynchronously processes a CrawlResult object to generate markdown content,
         summarize it if necessary, and split it into chunks.
-        Returns:
-            list[DB_Site]: A list containing a DB_Site object with the processed content,
-                           including the URL, markdown content, title, and chunks.
         """
         md = crawler_config.markdown_generator.generate_markdown(site.cleaned_html)
 
