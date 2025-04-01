ed422253a4563b93ddca7ba46a5de2165c8fc53c
Diego <dfal@cin.ufpe.br>
2025-03-31 22:39:19 -0300

    Add dialogs to chat page

--- frontend/src/components/Login.svelte
+++ frontend/src/components/Login.svelte
@@ -1,12 +1,80 @@
 <script lang="ts">
+    import { loginStatus, setLoggedIn } from "../api";
+    import { Dialog, DialogContent, DialogTrigger, DialogTitle, DialogDescription, DialogClose } from '../lib/components/ui/dialog';
     let activeTab = "";
+
+    let showLoginDialog = false;
+    let showRegisterDialog = false;
+
+    const handleLogIn = () => {
+    showLoginDialog = true;};
+
+  const handleRegister = () => {
+    showRegisterDialog = true;};
+
+    const confirmLogin = () => {
+    setLoggedIn(true);
+    showLoginDialog = false;
+  };
+
+  const confirmRegister = () => {
+    setLoggedIn(true);
+    showRegisterDialog = false;
+  };
+
+  const handleSignOut = () => {
+    setLoggedIn(false);
+    showRegisterDialog = false;
+    showLoginDialog = false;
+
+  };
 
 
 </script>
 
 <div class="login-register-container">
-    <button class="button is-primary" on:click={() => activeTab = "login"}>Login</button>
-    <button class="button is-link" on:click={() => activeTab = "register"}>Register</button>
+    {#if $loginStatus == false}
+
+<Dialog bind:open={showLoginDialog}>
+    <DialogTrigger>
+    <button class="button is-primary" on:click={handleLogIn}>Login</button>  
+    </DialogTrigger>
+<DialogContent>
+<DialogTitle>Login</DialogTitle>
+<DialogDescription>Fill the blanks with your credentials</DialogDescription>
+        <input type="text" placeholder="Username" class="border p-2 w-full rounded" />
+        <input type="password" placeholder="Password" class="border p-2 w-full rounded mt-2" />
+  <DialogClose>
+    <button on:click={confirmLogin} class="bg-purple-600 text-white px-4 py-2 rounded w-full mt-4">
+      Login
+    </button>
+        </DialogClose>
+</DialogContent>
+</Dialog>
+
+
+    <Dialog bind:open={showRegisterDialog}>
+     <DialogTrigger>
+        <button class="button is-link" on:click={handleRegister}>Register</button>
+    </DialogTrigger>
+    <DialogContent>
+      <DialogTitle>Register</DialogTitle>
+      <DialogDescription>Create an account</DialogDescription>
+      <input type="text" placeholder="Username" class="border p-2 w-full rounded" />
+      <input type="password" placeholder="Password" class="border p-2 w-full rounded mt-2" />
+      <DialogClose>
+        <button on:click={confirmRegister} class="bg-purple-600 text-white px-4 py-2 rounded w-full mt-4">
+          Create account
+        </button>
+      </DialogClose>
+    </DialogContent>
+  </Dialog>
+  
+    {:else}
+
+    <button class="button is-link" on:click={handleSignOut}>Sign Out</button>
+
+    {/if}
 </div>
 
 <style>
